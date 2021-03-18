8edc068644829041ee0b41f842873bea6457c679
Nathan <nathanhillyer@gmail.com>
2021-03-18 00:18:21 +0000

    First attempt at building the js file

--- js/script.js
+++ js/script.js
@@ -10,19 +10,74 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {
+    // first object
+    quote: 'One does not simply walk into Mordor.',
+    source: 'Boromir',
+    citation: 'Lord of the Rings: The Fellowship of the Ring',
+    year: 2001
+  },
+  {
+    // second object
+    quote: 'I am not a cat',
+    source: 'Keith Gill (AKA Roaring Kitty)',
+    year: 2021
+  },
+  {
+    // third object
+    quote: 'A lion does not concern himself with the opinion of sheep',
+    source: 'Tywin Lannister',
+  },
+  {
+    // fourth object
+    quote: 'I used to be an adventurer like you, then I took an arrow to the knee',
+    source: 'Whiterun Guard, Skyrim',
+    year: 2011
+  },
+  {
+    // fifth object
+    quote: 'Do. Or do not. There is no try',
+    source: 'Yoda',
+    citation: 'Star Wars: The Empire Strikes Back'
+  },
+  {
+    // add more objects if needed
+    quote: "It's-a me, Mario!",
+    source: 'Mario Mario'
+  }
+];
 
+/*
+  The getRandomQuote function should create a random number, and use that random number to return a random quote object from the quotes array.
+*/
 
+function getRandomQuote(arr) {
+  let randomNum = Math.floor(Math.random() * (arr.length)); // random number generated between 0 and the number elements in array
+  let randQuote = arr[randomNum];
+  return randQuote;
+}
 
-/***
- * `getRandomQuote` function
-***/
+/*
+  Perform 3 tasks: Call the getRandomQuote function, use the returned quote object to build a string of HTML and quote properties, then use that string to display a random quote in the browser.
+*/
+function printQuote() {
+  let quotation = getRandomQuote(quotes);
+  let html = `
+  <p class="quote">${quotation["quote"]}</p>
+  <p class="source">${quotation["source"]}
+  `; // build the HTML string. Leave off the last tag until end of possible data to be added
 
-
-
-/***
- * `printQuote` function
-***/
-
+  // if the objects have further properties, make sure they're displayed. Either way ensure closing <p> tag is added to end of string
+    if (quotation.citation) {
+      html += `<span class="citation">${quotation["citation"]}</span>`;
+    } 
+    if (quotation.year) {
+      html += `<span class="year">${quotation["year"]}</span>`;
+    }
+  html += `</p>`; 
+  document.getElementById('quote-box').innerHTML = html; // select the quote-box div and update it's HTML content with the random quote markup
+}
 
 
 /***
