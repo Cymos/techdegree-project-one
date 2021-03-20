8aaa1fade938f02fc1d2c616b30697920bd617bd
Nathan <nathanhillyer@gmail.com>
2021-03-20 12:22:30 +0000

    Added RGB changes
    RGB changes on timer or button click

--- js/script.js
+++ js/script.js
@@ -7,9 +7,6 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
 const quotes = [
   {
     // first object
@@ -54,10 +51,6 @@ const quotes = [
   }
 ];
 
-/*
-  The getRandomQuote function should create a random number, and use that random number to return a random quote object from the quotes array.
-*/
-
 // quotes does not need to be in the function definition as the scope allows it to be seen
 function getRandomQuote() {
   let randomNum = Math.floor(Math.random() * (quotes.length)); // random number generated between 0 and the number elements in array
@@ -65,9 +58,6 @@ function getRandomQuote() {
   return randQuote;
 }
 
-/*
-  Perform 3 tasks: Call the getRandomQuote function, use the returned quote object to build a string of HTML and quote properties, then use that string to display a random quote in the browser.
-*/
 function printQuote() {
   let randomQuote = getRandomQuote(quotes);
   let html = `
@@ -85,15 +75,20 @@ function printQuote() {
     if (randomQuote.tag) {
       html += `<span class="tag">${randomQuote.tag}</span>`;
     }
-  html += `</p>`; 
- 
+  html += `</p>`;
+  
+  document.body.style.backgroundColor = `${randomRGB(randomValue)}`;
   document.getElementById('quote-box').innerHTML = html; // select the quote-box div and update it's HTML content with the random quote markup
 
-  /*
-    NEXT STEPS:
-    1) Create a random function that updates the background color to a random color
-    2) Create a timing function with the setInterval() method to print a new quote to the page at regular intervals, like every 10 to 20 seconds
-  */
+}
+
+// reusing below 2 functions previously made in a Javascript Loops workspace - changed from arrow function to regular function
+function randomValue() {
+  return Math.floor(Math.random() * 256);
+}
+function randomRGB(value) {
+  const color = `rgb( ${value()}, ${value()}, ${value()} )`;
+  return color;
 }
 
 // code structure referenced from https://www.w3schools.com/jsref/met_win_setinterval.asp to remind me how to structure the method 
